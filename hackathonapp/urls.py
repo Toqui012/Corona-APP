03d05cfd30a2b6f60c719ce2f9e74d494da5bfeb
Toqui012 <alvaroseba2000@gmail.com>
2020-06-27 07:10:39 -0400

    Graficos con 3 dias

--- hackathonapp/urls.py
+++ hackathonapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('upload',views.uploadData, name='uploadData')
+    path('',views.LineChartJSONView, name='line_chart_json'),
+    path('upload',views.uploadData, name='uploadData'),
+    path('chart', views.line_chart, name='line_chart'),
+    path('chartJSON', views.line_chart_json, name='line_chart_json'),
+    path('grafico',views.grafico, name='grafico'),
 ]
