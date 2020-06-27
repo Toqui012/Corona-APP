24a9bb5415509a9f1949d497d62bc49d1484f2e0
Toqui012 <alvaroseba2000@gmail.com>
2020-06-27 16:16:00 -0400

    presentación primer pitch

--- hackathonapp/urls.py
+++ hackathonapp/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('chart', views.line_chart, name='line_chart'),
     path('chartJSON', views.line_chart_json, name='line_chart_json'),
     path('grafico',views.grafico, name='grafico'),
+    path('question',views.question, name='question'),
 ]
