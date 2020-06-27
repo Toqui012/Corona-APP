b675498e71420649a035bd3b25615b3dc1337d89
Juan Muñoz <cl.jmunoz@gmail.com>
2020-06-26 21:38:32 -0400

    Carga de modelos

--- hackathonapp/models.py
+++ hackathonapp/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+import uuid 
 
-# Create your models here.
+class news(models.Model):
+    titular = models.CharField(max_length=200,primary_key=True,default=None)
+    descripcion = models.TextField(max_length=500, null=None)
+    fuente = models.CharField(max_length=200, default=None)
+
+class errorTable(models.Model):
+    idError = models.DateField(auto_now=True)
+    Error = models.CharField(max_length = 800, null=False)
+
+class district(models.Model):
+    Codregion = models.CharField(max_length=2, default=None, primary_key=True)
+    RegionName = models.CharField(max_length=50, default=None)
+    Area = models.FloatField(default = None)
+    Lat = models.FloatField(default = None)
+    Long = models.FloatField(default = None)
+    Population = models.FloatField(default = None)
+    
+    def __str__(self):
+        return self.RegionName
+    
+class commune(models.Model):
+    CodComuna = models.IntegerField(primary_key=True, default=None)
+    Reg = models.ForeignKey(district, on_delete=models.CASCADE)
+    ComunaName = models.CharField(max_length=50, default=None)
+    Poblation = models.FloatField(default=None)
+
+    def __str__(self):
+        return self.ComunaName
+    
+class activesCases(models.Model):
+    AcCod_comuna = models.ForeignKey(commune, on_delete=models.CASCADE)
+    AcDate = models.DateField(auto_now=False, auto_now_add=False, default=None)
+    AcCantidad = models.FloatField(default=None)
+
+    def __str__(self):
+        return self.AcCod_comuna
+
+class reports(models.Model):
+    RDate = models.DateField(auto_now=False, auto_now_add=False, default=None)
+    RComuna = models.ForeignKey(commune, on_delete=models.CASCADE)
+    RConfirmed = models.FloatField(default=None)
+    RActive = models.FloatField(default=None)
+    RRecovered = models.FloatField(default=None)
+    
+    def get_commune(self):
+        return self.RComuna.ComunaName
+    
+    def get_district(self):
+        return self.RComuna.Reg.RegionName
+    
+    class Meta:
+        ordering = ["-RDate"]
+
+class deathsbyDistrict(models.Model):
+    DDate = models.DateField(auto_now=False, auto_now_add=False, default=None)
+    DCodRegion = models.ForeignKey(district, on_delete=models.CASCADE)
+    Ddeaths = models.FloatField(default=None)
+
+
+class reportDate(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    RDDate = models.DateField(auto_now=False, auto_now_add=False, default=None)
--- hackathonapp/views.py
+++ hackathonapp/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.template.context_processors import request
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-
-# Create your views here.
-
+from hackathonapp.models import news, errorTable, district, commune, activesCases, reports, deathsbyDistrict, reportDate
 
 def index(request):
     return render(request,'index.html')
+
