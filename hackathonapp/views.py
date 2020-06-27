43f5278eeb9dd76df42f996d0eae6629c09c958b
Toqui012 <alvaroseba2000@gmail.com>
2020-06-26 20:01:52 -0400

    Commit Base

---
+++ hackathonapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.template.context_processors import request
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,'index.html')
